5dae618f6adb6ba3793cd6bcc337729f48e6baa3
raymondfam <raymondfam@gmail.com>
2022-08-22 04:47:23 -0500

    add basepath and assetprefix and add export command

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        loader: "akamai",
+        path: "",
+    },
+    basePath: "/defi-nft-marketplace-mumbai",
+    assetPrefix: "/defi-nft/marketplace-mumbai",
 }
 
 module.exports = nextConfig
